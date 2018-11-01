a9239eee186eebd20b54e6418413098504f20c45
Isam Hashmi <isam@techrxservices.com>
2018-10-31 22:51:33 -0500

    Added Allergies functionality and fixed css

--- scripts.js
+++ scripts.js
@@ -6,6 +6,8 @@ var ingredient;
 function AddIngredient()
 {
 
+
+
     ingredient = $('#ingredients-input').val();
     if (!currentList.includes(ingredient) && ingredient !== "")
     {
@@ -17,6 +19,9 @@ function AddIngredient()
             )
         );
 
+
+        if (currentList.length >= 1)
+            $("#submitIngredients").show();
 
         $('#ingredients-input').val("");
     }
@@ -42,6 +47,8 @@ function DeleteIngredient()
     {
         $("#delete-button").hide();
     }
+    if (currentList.length < 1)
+        $("#submitIngredients").hide();
 }
 
 
@@ -76,25 +83,40 @@ $(document).ready(function ()
 });
 
 
- 
 
 function SubmitIngredients()
 {
     $("#FoodTableBody tr").remove();
+
+    let allergies = [];
+    $("input[name='allergy']:checked").each(function (index, domElement)
+    {
+       allergies.push($(domElement).val());
+    });
+    console.log(allergies);
+
+    var link = "https://api.edamam.com/search?q=" + currentList.join(",") + "&app_id=9d10b7ba&app_key=d89e9b03886069bab9bac13fa8189398&from=0&to=100"; //Link to get regular link
+
+    if (allergies.length > 0)
+    {
+        link = "https://api.edamam.com/search?q=" + currentList.join(",") + "&app_id=9d10b7ba&app_key=d89e9b03886069bab9bac13fa8189398&from=0&to=100" + "&health=" + allergies.join(","); //Link to get allergies link
+    }
+
+
     $.ajax(
     {
-        cache: false, //Accessing into the API
-        url: "https://api.edamam.com/search?q=" + currentList.join(",") + "&app_id=9d10b7ba&app_key=d89e9b03886069bab9bac13fa8189398&from=0&to=100",
+            cache: false, //Accessing into the API
+            url: link,
         success: function (data, textStatus, xhr)
         {
             array = xhr.responseJSON;
-         //   console.log(array);
+           console.log(array);
             for (var x = 0; x < array.hits.length; x++)
             {
                 //console.log("x: " + x);
                 $("#FoodTableBody").append(
                     $('<tr>').append(
-                        $("<td class=\"col-lg-4\">").append(x + ") " + array.hits[x].recipe.label + "\n")  
+                        $("<td class=\"col-lg-4\">").append( array.hits[x].recipe.label + "\n")  
                     ).append(
                         $("<td class=\"col-lg-4\">").append(
                             $("<a>").attr("href", array.hits[x].recipe.url).attr("target", "_blank").append("Click me")
