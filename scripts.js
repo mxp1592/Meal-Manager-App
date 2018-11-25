24184d78f8fe386f1211631c6f37f19db9d47e42
Michael Park <michaelpark8910@gmail.com>
2018-11-25 02:36:21 -0600

    - Added and created Filters - Added and made a column for "Recipe Health Information"

--- scripts.js
+++ scripts.js
@@ -3,14 +3,12 @@ var currentList = [];
 var ingredient;
 
 //Click Add Ingredient button
-function AddIngredient()
-{
+function AddIngredient() {
 
 
 
     ingredient = $('#ingredients-input').val();
-    if (!currentList.includes(ingredient) && ingredient !== "")
-    {
+    if (!currentList.includes(ingredient) && ingredient !== "") {
         currentList.push(ingredient);
 
         $("#ingredient-list-section").append(
@@ -25,26 +23,22 @@ function AddIngredient()
 
         $('#ingredients-input').val("");
     }
-    else
-    {
+    else {
         $('#ingredients-input').val("");
     }
-       
+
 
 }
 
-function DeleteIngredient()
-{
+function DeleteIngredient() {
     ingredient = $('.selected');
     ingredient.closest("tr").remove();
-              
+
     remove(currentList, ingredient[0].cells[0].innerText);
-    if ($(".selected").length > 0)
-    {
+    if ($(".selected").length > 0) {
         $("#delete-button").show();
     }
-    else
-    {
+    else {
         $("#delete-button").hide();
     }
     if (currentList.length < 1)
@@ -52,30 +46,24 @@ function DeleteIngredient()
 }
 
 
-function remove(arr, what)
-{
+function remove(arr, what) {
     var found = arr.indexOf(what);
 
-    while (found !== -1)
-    {
+    while (found !== -1) {
         arr.splice(found, 1);
         found = arr.indexOf(what);
     }
 }
 
 
-$(document).ready(function ()
-{
-    $('#ingredient-list-section').on('click', 'tr', function ()
-    {
+$(document).ready(function () {
+    $('#ingredient-list-section').on('click', 'tr', function () {
         $(this).toggleClass('selected').siblings().removeClass('selected');
 
-        if ($(".selected").length > 0)
-        {
+        if ($(".selected").length > 0) {
             $("#delete-button").show();
         }
-        else
-        {
+        else {
             $("#delete-button").hide();
         }
 
@@ -84,52 +72,96 @@ $(document).ready(function ()
 
 
 
-function SubmitIngredients()
-{
+function SubmitIngredients() {
     $("#FoodTableBody tr").remove();
 
     let allergies = [];
-    $("input[name='allergy']:checked").each(function (index, domElement)
-    {
-       allergies.push($(domElement).val());
+    $("input[name='allergy']:checked").each(function (index, domElement) {
+        allergies.push($(domElement).val());
     });
     console.log(allergies);
 
     var link = "https://api.edamam.com/search?q=" + currentList.join(",") + "&app_id=9d10b7ba&app_key=d89e9b03886069bab9bac13fa8189398&from=0&to=100"; //Link to get regular link
 
-    if (allergies.length > 0)
-    {
+    if (allergies.length > 0) {
         link = "https://api.edamam.com/search?q=" + currentList.join(",") + "&app_id=9d10b7ba&app_key=d89e9b03886069bab9bac13fa8189398&from=0&to=100" + "&health=" + allergies.join(","); //Link to get allergies link
     }
 
+    let fatsVal = parseFloat($("#Fats").val());
+    let carbsVal = parseFloat($("#Carbs").val());
+    let proteinsVal = parseFloat($("#Proteins").val());
 
     $.ajax(
-    {
+        {
             cache: false, //Accessing into the API
             url: link,
-        success: function (data, textStatus, xhr)
-        {
-            array = xhr.responseJSON;
-           console.log(array);
-            for (var x = 0; x < array.hits.length; x++)
+            success: function (data, textStatus, xhr)
             {
-                //console.log("x: " + x);
-                $("#FoodTableBody").append(
-                    $('<tr>').append(
-                        $("<td class=\"col-lg-4\">").append( array.hits[x].recipe.label + "\n")  
-                    ).append(
-                        $("<td class=\"col-lg-4\">").append(
-                            $("<a>").attr("href", array.hits[x].recipe.url).attr("target", "_blank").append("Click me")
-                        )  
-                    ).append(
-                        $("<td class=\"col-lg-4\">").append($("<img>").attr("src", array.hits[x].recipe.image)
-                    )
+                array = xhr.responseJSON;
+                console.log(array);
+                let foods = [];
 
-                        //
-                    
-                ));
+                for (var x = 0; x < array.hits.length; x++) {
+                    foods.push(array.hits[x]); // Put all the hits into the foods array
+                }
+
+                let filters = [];
+                $("input[name='filters']:checked").each(function (index, domElement) {
+                    filters.push($(domElement).val());
+                }
+                );
+                
+                if (filters.includes("fats"))
+                {
+                    if ($("#fatSelection").val() == "Less than")
+                    {
+                        foods = foods.filter(function (x) { return parseFloat(x.recipe.digest[0].total) < fatsVal });
+                    }
+                    else 
+                    {
+                        foods = foods.filter(function (x) { return parseFloat(x.recipe.digest[0].total) > fatsVal });
+                    }
+                }
+                if (filters.includes("carbs") && $("#carbSelection").val() == "Less than") 
+                {
+                    foods = foods.filter(function (x) { return parseFloat(x.recipe.digest[1].total) < carbsVal });
+                }
+                else if (filters.includes("carbs") && $("#carbSelection").val() == "Greater than")
+                {
+                    foods = foods.filter(function (x) { return parseFloat(x.recipe.digest[1].total) > carbsVal });
+                }
+                
+                if (filters.includes("proteins") && $("#proteinSelection").val() == "Less than")
+                {
+                    foods = foods.filter(function (x) { return parseFloat(x.recipe.digest[2].total) < proteinsVal });   
+                }
+                else if (filters.includes("proteins") && $("#proteinSelection").val() == "Greater than")
+                { 
+                    foods = foods.filter(function (x) { return parseFloat(x.recipe.digest[2].total) > proteinsVal });  
+                }
+              
+
+
+                for (var x = 0; x < foods.length; x++) {
+                    $("#FoodTableBody").append(
+                        $('<tr>').append(
+                            $("<td class=\"col-lg-3\">").append(foods[x].recipe.label + "\n")
+                        ).append(
+                            $("<td class=\"col-lg-3\">").append(
+                                $("<a>").attr("href", foods[x].recipe.url).attr("target", "_blank").append("Click me")
+                            )
+                        ).append(
+                            $("<td class=\"col-lg-3\">").append($("<p>").append("Fat: " + foods[x].recipe.digest[0].total.toFixed(2) + foods[x].recipe.digest[0].unit))
+                                .append($("<p>").append("Carb: " + foods[x].recipe.digest[1].total.toFixed(2) + foods[x].recipe.digest[1].unit))
+                                .append($("<p>").append("Protein: " + foods[x].recipe.digest[2].total.toFixed(2) + foods[x].recipe.digest[2].unit))
+                        ).append(
+                            $("<td class=\"col-lg-3\">").append($("<img>").attr("src", foods[x].recipe.image)
+                            ))
+                    );
+                }
             }
-        }
-    });
+
+
+        });
 }
 
